806ea7e53f2cfe6e453d9989dfee5cf456e19615
Aluno <aluno@IPIRANGA.FATEC>
2022-04-07 17:44:56 -0300

    Atualização private

--- Jogo.java
+++ Jogo.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class Jogo {
     public static void main(String[] args) throws Exception {
         Personagem cacador = new Personagem(); //criacao de objeto com variavel de referencia (cacador)
-        cacador.nome = "Cacador";
+        cacador.setNome("Cacador");
         Random gerador = new Random();
         while(true){
             int oQueFazer = gerador.nextInt(3)+1; // [1,3]
--- Personagem.java
+++ Personagem.java
@@ -1,22 +1,29 @@
 public class Personagem {
-    String nome; //variavel de instancia e referencia
-    int energia; //variaveis de instancia e primitivas
-    int fome;
-    int sono;
+    private String nome; //variavel de instancia e referencia
+    private int energia = 10; //variaveis de instancia e primitivas
+    private int fome = 0;
+    private int sono = 0;
 
-    final static int LIMITE_SUPERIOR = 10; //constante, não da pra trocar esse valor, final quer dizer que nao pode atribuir
+    private final static int LIMITE_SUPERIOR = 10; //constante, não da pra trocar esse valor, final quer dizer que nao pode atribuir
     //coloque o static para nao puxar essa memo pra cada objeto
     //static significa é dessa classe, não crie varias para cada objeto, instancias
     //valor compartilhado pra todas as instancias, uma copia para todos os objetos
     //para atribuir pode Personagem.constante = .. porque é da classe e nao das instancias
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
 
     //metodo de instancia
     void cacar(){
         if (this.energia >= 2) {
             System.out.println(this.nome + " cacando...");
             this.energia -= 2;
-            this.fome = Math.min (this.fome +1, 10);
-            this.sono = Math.min(this.sono +1, 10);
+            this.fome = Math.min (this.fome +1, LIMITE_SUPERIOR);
+            this.sono = Math.min(this.sono +1, LIMITE_SUPERIOR);
         }
         else{
             System.out.println(this.nome + " sem energia para cacar... ");
@@ -27,7 +34,7 @@ public class Personagem {
         if(this.fome >= 1){
             System.out.printf("%s comendo...", this.nome);
             this.fome -= 1;
-            this.energia = this.energia + 1 <= 10 ? this.energia + 1 : this.energia;
+            this.energia = this.energia + 1 <= LIMITE_SUPERIOR ? this.energia + 1 : this.energia;
         }
         else{
             System.out.printf("%s sem fome...\n", this.nome);
@@ -39,7 +46,7 @@ public class Personagem {
         if(this.sono >= 1){
             System.out.println(String.format("%s dormindo...", this.nome));
             this.sono -= 1;
-            this.energia = this.energia + 1 <=10 ? this.energia +1 : this.energia;
+            this.energia = this.energia + 1 <=LIMITE_SUPERIOR ? this.energia +1 : this.energia;
         }
         else{
             System.out.println(nome + "sem sono...");
